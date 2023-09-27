00c83ed70af1288fd3ca534d69b855716261090c
Build Service <buildservice@mvpprojects.com>
2023-09-27 16:10:32 +0200

    udpate

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ public static class Program
     public static void Main(string[] args)
     {
         //new Example01().Run();
-        //new Example02().Run();
-        new Example03().Run();
+        new Example02().Run();
+        //new Example03().Run();
     }
 }
